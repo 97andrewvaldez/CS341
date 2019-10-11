117781c94cd266d8202fbf192c45971c6bd9e267
Carl Atwell <carl.j.atwell@gmail.com>
2019-10-10 20:31:07 -0500

    A better general login GUI

--- GUI.py
+++ GUI.py
@@ -3,22 +3,45 @@ import kivy
 kivy.require('1.11.1')
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 
-class LoginScreen(GridLayout):
-    def p(self):
-        print(self.username)
-        print(self.password)
-
+class LoginScreen(FloatLayout):
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text = 'Who is you daddy?'))
-        self.username = TextInput(multiline = False)
+        # self.GridLayout(cols=2, row_force_default=True, row_default_height=40)
+        # self.FloatLayout(size = (300, 300))
+        self.size = (50, 50)
+        # the enter button
+        self.button = Button(text = 'Enter',
+                        size_hint = (.25, .10),
+                        pos_hint = {'x':.37, 'y':.4}
+                        # pos = (300, 300)
+                            )
+        self.add_widget(self.button)
+
+        # The username prompt label
+        self.username_label = Label(text = 'Username: ',
+                                size_hint = (.25, .10),
+                                pos_hint = {'x': .10, 'y':.80})
+        self.add_widget(self.username_label)
+
+        # the username textbox
+        self.username = TextInput(multiline = False,
+                        size_hint = (.3, .05),
+                        pos_hint = {'x': .35, 'y': .825})
         self.add_widget(self.username)
-        self.add_widget(Label(text = 'And what does he do?'))
-        self.password = TextInput(password = True, multiline = False)
+
+        # the password prompt label
+        self.password_label = Label(text = 'Password: ',
+                                size_hint = (.25, .10),
+                                pos_hint = {'x': .10, 'y':.70})
+        self.add_widget(self.password_label)
+
+        self.password = TextInput(multiline = False,
+                        size_hint = (.3, .05),
+                        pos_hint = {'x': .35, 'y': .725})
         self.add_widget(self.password)
 
 class MyApp(App):
